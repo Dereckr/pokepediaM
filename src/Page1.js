acfcad9df21c2a278c51a56dc8ae31886fa7b262
Dereckr <dereckromos@gmail.com>
2024-10-22 22:28:37 -0400

    update

--- src/Page1.js
+++ src/Page1.js
@@ -1,14 +1,49 @@
-import { createElement } from './utils';
+import { createElement } from "./utils";
 
 function Page1() {
-  const title = createElement('h2', { textContent: 'Page 1' });
-
-  const page3Link = createElement('a', {
-    href: '/#/page3',
-    textContent: 'Link to Page 3',
+  const title = createElement("h2", { textContent: "Search" });
+  const label1 = createElement("label", {
+    textContent: "Pokemon Name: ",
+    className: "label1",
   });
 
-  return createElement('div', {}, [title, page3Link]);
+  const input1 = createElement("input", {
+    name: "pokeName",
+    textContent: "or",
+  });
+  const label2 = createElement("label", {
+    textContent: "Pokemon Number: ",
+    className: "label2",
+  });
+  const input2 = createElement("input", { name: "pokeNumber" });
+  const searchButtonName = createElement("button", {
+    type: "submit",
+    value: "submit",
+    className: "searchButton",
+    textContent: "Search by Name",
+  });
+  const searchButtonNumber = createElement("button", {
+    type: "submit",
+    value: "submit",
+    className: "searchButton",
+    textContent: "Search by Number",
+  });
+
+  // const page3Link = createElement("a", {
+  //   href: "/#/page3",
+  //   textContent: "Link to Page 3",
+  // });
+
+  return createElement("div", {}, [
+    title,
+    // page3Link,
+    label1,
+    input1,
+    label2,
+    input2,
+    searchButtonName,
+    searchButtonNumber,
+  ]);
 }
 
 export default Page1;
