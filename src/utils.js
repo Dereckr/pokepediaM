3d43357c90905156fb79632a3ff87ebc5fdf0f57
Dereckr <dereckromos@gmail.com>
2024-10-23 21:52:58 -0400

    update

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ export function createElement(type, props = {}, children = []) {
 
   // props: {textContent: "Hello world!", id: "header1", "data-productId": 123, ...}
   Object.entries(props).forEach(([key, value]) => {
-    if(~key.indexOf('-')) {
+    if (~key.indexOf("-")) {
       element.setAttribute(key, value); // data attributes
     } else {
       element[key] = value;
@@ -16,3 +16,26 @@ export function createElement(type, props = {}, children = []) {
 
   return element;
 }
+
+async function getPokemon() {
+  const url = "https://pokeapi.co/api/v2/pokemon/";
+
+  const response = await fetch(url);
+  //check to see if the fetch was successful
+  if (response.ok) {
+    // the API will send us JSON...but we have to convert the response before we can use it
+    // .json() also returns a promise...so we await it as well.
+    const data = await response.json();
+    doStuff(data);
+  }
+}
+function doStuff(data) {
+  let results = null;
+  results = data;
+  results.results.forEach((pokemon) => {
+    const div = document.createElement("div");
+    div.textContent = pokemon.name;
+    document.querySelector("main").appendChild(div);
+  });
+}
+getPokemon();
